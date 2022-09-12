ffb63866a06fdc34e2cbd7b3b12a4cac85582988
vasylcode <vasylcode@gmail.com>
2022-09-12 22:51:29 +0200

    txt upd

--- install.sh
+++ install.sh
@@ -74,7 +74,7 @@ if [ "$language" = "uk" ]; then
     fi
 else
   if [[ `service suid status | grep active` =~ "running" ]]; then
-      echo -e "\n\e[93mSui Fullnode Updated\e[0m\n"
+      echo -e "\n\e[93mSui Fullnode Installed\e[0m\n"
       echo -e "Check node logs \e[92mjournalctl -n 100 -f -u sui\e[0m"
       echo -e "\e[92mCTRL + C\e[0m to exit logs\n"
       echo -e "Open sui menu \e[92msui client\e[0m"
