cca4f4ecbc59e9cdb0bafa2f0712b2f209da337e
vasylcode <vasylcode@gmail.com>
2023-01-20 12:35:37 +0100

    latest from releases added

--- devnet_update.sh
+++ devnet_update.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
+if ! command -v curl &> /dev/null && ! command -v git &> /dev/null && ! command -v jq &> /dev/null; then
+    sudo apt install -y curl git jq
+fi
+
 echo -e "\n\e[93mUpdate Started...\e[0m\n"
 systemctl stop suid
 rm -rf /var/sui/db/* /var/sui/genesis.blob $HOME/sui
 source $HOME/.cargo/env
+RELEASE_INFO=$(curl --silent https://api.github.com/repos/MystenLabs/sui/releases/latest)
+TAG_NAME=$(echo $RELEASE_INFO | jq -r '.tag_name')
 cd $HOME
 git clone https://github.com/MystenLabs/sui.git
 cd sui
-git checkout devnet-0.21.1
+git checkout $TAG_NAME
 cargo build --bin sui-node --bin sui --release
 mv ~/sui/target/release/sui-node /usr/local/bin/
 mv ~/sui/target/release/sui /usr/local/bin/
@@ -16,13 +22,13 @@ systemctl restart suid
 
 
 if [ "$language" = "uk" ]; then
-    echo -e "\n\e[93mSui Devnet Fullnode Оновлена\e[0m\n"
+    echo -e "\n\e[93mSui $TAG_NAME Оновлено\e[0m\n"
     echo -e "Подивитись логи ноди \e[92mjournalctl -n 100 -f -u suid\e[0m"
     echo -e "\e[92mCTRL + C\e[0m щоб вийти з логів\n"
     echo -e "Зайти в клієнт ноди \e[92msui client\e[0m"
     echo -e "Подивитись адресу гаманця \e[92msui keytool list\e[0m"
 else
-    echo -e "\n\e[93mSui Devnet Fullnode Updated\e[0m\n"
+    echo -e "\n\e[93mSui $TAG_NAME Updated\e[0m\n"
     echo -e "Check node logs \e[92mjournalctl -n 100 -f -u suid\e[0m"
     echo -e "\e[92mCTRL + C\e[0m to exit logs\n"
     echo -e "Open sui menu \e[92msui client\e[0m"
